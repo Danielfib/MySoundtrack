8688524d5e41ef501963729fa2a94d7f5c6482da
Daniel Filgueira <danielfilguera98@gmail.com>
2021-07-10 11:00:54 -0300

    including demo and documentatino

---
+++ MySoundtrack/Assets/CreativeSoundtrack/Demo/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    private float frontMovement;
+    private float sideMovement;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        frontMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        sideMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.Translate(sideMovement, 0, frontMovement);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ MySoundtrack/Assets/CreativeSoundtrack/Demo/MouseCamLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamLook : MonoBehaviour {
+
+    [SerializeField]
+    public float sensitivity = 5.0f, smoothing = 2.0f;
+    public GameObject character;
+    private Vector2 mouseLook, smoothVector;
+
+	void Start () {
+        character = this.transform.parent.gameObject;
+	}
+	
+	void Update () {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothVector.x = Mathf.Lerp(smoothVector.x, md.x, 1f / smoothing);
+        smoothVector.y = Mathf.Lerp(smoothVector.y, md.y, 1f / smoothing);
+        mouseLook += smoothVector;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
